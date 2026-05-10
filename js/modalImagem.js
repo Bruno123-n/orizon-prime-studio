b09e0207766c8f02c2a5973c3a1263130a98a6db
Bruno123-n <hungriakaa@gmail.com>
2026-05-10 17:38:25 -0300

    git add . git commit -m "feat: Add responsiveness and modal functionality."

--- js/modalImagem.js
+++ js/modalImagem.js
@@ -1,10 +1,20 @@
 export function modalImagem() {
 
+    
     const imagens = document.querySelectorAll('.galeria-imagem img, .sobre-imagem img')
-
+    
     const modal = document.querySelector('.modal-imagem')
-
+    
     const imagemModal = modal.querySelector('img')
+    
+    const fecharModal = document.querySelector('.fechar-modal')
+    
+
+    function fecharModalFunc() {
+        modal.classList.remove('ativo')
+        document.body.classList.remove('modal-aberto')
+    }
+
 
     imagens.forEach((imagem) => {
 
@@ -12,16 +22,36 @@ export function modalImagem() {
 
             modal.classList.add('ativo')
 
+            document.body.classList.add('modal-aberto')
+
             imagemModal.src = imagem.src
 
         })
 
     })
 
-    modal.addEventListener('click', () => {
+    document.addEventListener('keydown', (event) => {
 
-        modal.classList.remove('ativo')
+        if(event.key === 'Escape') {
+
+            fecharModalFunc()
+
+        }
 
     })
+
+    modal.addEventListener('click', (event) => {
+
+        if(event.target === modal) {
+
+            fecharModalFunc()
+
+        }
+
+    })
+
+
+        fecharModal.addEventListener('click', fecharModalFunc)
+    
 
 }
